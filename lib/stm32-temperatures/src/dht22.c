2dc8a208e6ddcdfc36098c6ea57b80515f48c467
Leandro Loureiro <leandroloureiro@protonmail.com>
2021-06-06 13:09:00 -0500

    fixing identation

--- lib/stm32-temperatures/src/dht22.c
+++ lib/stm32-temperatures/src/dht22.c
@@ -5,14 +5,14 @@
 
 void DHT22_Start(void) {
 
-	Set_Pin_Output(DHT22_PORT, DHT22_PIN); // set the pin as output
-	HAL_GPIO_WritePin(DHT22_PORT, DHT22_PIN, 0);   // pull the pin low
-	delay(1200);   // wait for > 1ms
+  Set_Pin_Output(DHT22_PORT, DHT22_PIN); // set the pin as output
+  HAL_GPIO_WritePin(DHT22_PORT, DHT22_PIN, 0);   // pull the pin low
+  delay(1200);   // wait for > 1ms
 
-	HAL_GPIO_WritePin (DHT22_PORT, DHT22_PIN, 1);   // pull the pin high
-	delay(20);   // wait for 30us
+  HAL_GPIO_WritePin (DHT22_PORT, DHT22_PIN, 1);   // pull the pin high
+  delay(20);   // wait for 30us
 
-	Set_Pin_Input(DHT22_PORT, DHT22_PIN);   // set as input
+  Set_Pin_Input(DHT22_PORT, DHT22_PIN);   // set as input
 
 }
 
@@ -20,64 +20,64 @@ uint8_t DHT22_Check_Response(void) {
 
   uint8_t response = 0;
 
-	Set_Pin_Input(DHT22_PORT, DHT22_PIN);   // set as input
-	
-	delay(40);  // wait for 40us
-	if (!(HAL_GPIO_ReadPin (DHT22_PORT, DHT22_PIN))) { // if the pin is low
-	
-		delay(80);   // wait for 80us
+  Set_Pin_Input(DHT22_PORT, DHT22_PIN);   // set as input
+    
+  delay(40);  // wait for 40us
+  if (!(HAL_GPIO_ReadPin (DHT22_PORT, DHT22_PIN))) { // if the pin is low
+    
+    delay(80);   // wait for 80us
 
     response = HAL_GPIO_ReadPin(DHT22_PORT, DHT22_PIN) ? 1 : -1;  // if the pin is high, response is ok
 
-	}
+  }
 
-	while ((HAL_GPIO_ReadPin(DHT22_PORT, DHT22_PIN)));   // wait for the pin to go low
+  while ((HAL_GPIO_ReadPin(DHT22_PORT, DHT22_PIN)));   // wait for the pin to go low
 
-	return response;
+  return response;
 
 }
 
 uint8_t DHT22_Read(void) {
 
-	uint8_t i, j;
+  uint8_t i, j;
 
-	for(j=0; j<8; j++) {
+  for(j=0; j<8; j++) {
 
-		while (!(HAL_GPIO_ReadPin(DHT22_PORT, DHT22_PIN)));   // wait for the pin to go high
-		delay(40);   // wait for 40 us
+    while (!(HAL_GPIO_ReadPin(DHT22_PORT, DHT22_PIN)));   // wait for the pin to go high
+    delay(40);   // wait for 40 us
 
-		if(!(HAL_GPIO_ReadPin(DHT22_PORT, DHT22_PIN))) {   // if the pin is low
-			i&= ~(1<<(7-j));   // write 0
-		} else {
-		  i|= (1<<(7-j));  // if the pin is high, write 1
+    if(!(HAL_GPIO_ReadPin(DHT22_PORT, DHT22_PIN))) {   // if the pin is low
+      i&= ~(1<<(7-j));   // write 0
+    } else {
+      i|= (1<<(7-j));  // if the pin is high, write 1
     } 
 
-		while ((HAL_GPIO_ReadPin(DHT22_PORT, DHT22_PIN)));  // wait for the pin to go low
+    while ((HAL_GPIO_ReadPin(DHT22_PORT, DHT22_PIN)));  // wait for the pin to go low
 
-	}
+    }
 
-	return i;
+    return i;
   
 }
 
 void dht22_Get_Temperature_Humidity(float *temperature, float *humidity) {
 
-    uint8_t temperature_byte1, temperature_byte2, humidity_byte1, humidity_byte2;
-    uint16_t temp_temperature, temp_humidity; 
+  uint8_t temperature_byte1, temperature_byte2, humidity_byte1, humidity_byte2;
+  uint16_t temp_temperature, temp_humidity; 
 
-	  DHT22_Start();
-	  DHT22_Check_Response();
-	  humidity_byte1 = DHT22_Read();
-    humidity_byte2 = DHT22_Read();
-	  temperature_byte1 = DHT22_Read();
-	  temperature_byte2 = DHT22_Read();
-	  DHT22_Read();
+  DHT22_Start();
+  DHT22_Check_Response();
+  humidity_byte1 = DHT22_Read();
+  humidity_byte2 = DHT22_Read();
+  temperature_byte1 = DHT22_Read();
+  temperature_byte2 = DHT22_Read();
+  DHT22_Read();
 
-	  temp_temperature = (temperature_byte1 << 8) | temperature_byte1;
-	  temp_humidity = (humidity_byte1 << 8) | humidity_byte2;
+  temp_temperature = (temperature_byte1 << 8) | temperature_byte1;
+  temp_humidity = (humidity_byte1 << 8) | humidity_byte2;
 
-	  *temperature = (float) (temp_temperature / 10.0);
-	  *humidity = (float) (temp_humidity / 10.0);
+  *temperature = (float) (temp_temperature / 10.0);
+  *humidity = (float) (temp_humidity / 10.0);
 
 }
 
